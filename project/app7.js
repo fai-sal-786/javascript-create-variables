396c2becb3f005c8f1dac32116d1ff8811f1181d
fai-sal-786 <154748475+fai-sal-786@users.noreply.github.com>
2024-04-29 09:28:25 -0700

    create variable in javascript

---
+++ project/app7.js
@@ -0,0 +1,224 @@
+// Q1: Given var  x = 25 and  var y = 7 , what is the result of x % y?
+
+// var  x = 25;
+// var  y = 7;
+// var result = x % y;
+// console.log (result);
+
+// answer: 4 reminder
+
+
+
+
+// Q2: if  var  a = 15 and var b = 4 , what is the value  of a * b after adding 10 to a and subtracting 2 from b?
+
+// initial value
+// var a = 15;
+// var b = 4;
+
+// updated value
+// a = a + 10;
+// b = b - 2;
+
+
+// var result = a * b;
+// console.log("The value of a * b  after adding 10 to a and subtracting 2 b is: ", result);
+
+// answer: 50;
+
+
+
+// Q3:suppose var c = 12 and var d = 3 , calculate the result of c ** d?
+
+// var c = 12;
+// var d = 3;
+// var result = Math.pow(c,  d);
+
+// console.log('The result of c  ** d is:', result);
+
+// answer: 1728;
+
+
+
+// Q4: for var e = 10 and var f = 3 ,  what is the value of e += f * 2?
+
+// initial values:
+// var e = 10;
+// var f = 3;
+// var result = f * 2;
+
+// e+= result;
+// console.log("The value of e += f * 2 is : ", e);
+
+// answer: 16;
+
+
+
+
+
+
+
+// Q5: considering var g = 20 and var h = 4, what willbe the outcome of g/h after adding 5 to g?
+
+// initial value:
+
+// var g = 20;
+// var h = 4;
+
+// g+= 5;
+
+// var result = g / h;
+// console.log("The value of g / h after adding 5  to g is:", result);
+
+// answer: 6.25;
+
+
+
+
+// Q6:Given var i = 8 and var j = 3, determine the value of i * j after subtracting 2 from i?
+
+// var i = 8;
+// var j = 3;
+
+// i-= 2;
+// var result = i * j;
+// console.log("The value of i * j after subtracting 2 from i is: ", result);
+
+// answer: 18;
+
+
+
+
+// Q7:if var k = 18 and  var l  = 5; what is the value of k - 1 + 2?
+
+// var k = 18;
+// var l = 5;
+// var result = k - 1 + 2;
+// console.log("The value of k - 1 + 2 is:", result);
+
+// answer: 19;
+
+
+
+
+
+// Q8: Suppose var m = 7 and var n = 2 , calculate the value of m ** n?
+
+// var m = 7;
+// var n = 2;
+// var result = Math.pow(m, n);
+// console.log("The value of m ** n is : ", result);
+
+// answer: 49;
+
+
+
+
+
+
+
+// Q9: for var o = 13 and var p = 4 , what will be the outcome of o % (p + 2)?
+
+// var o = 13;
+// var p = 4;
+// var result = p + 2;
+// console.log("The result of p + 2 is :", result);
+
+// answer: 6;
+
+
+
+
+
+
+
+// Q10:Given var q = 16 and var r = 3, determine the result of q * r after dividing q by 2?
+
+// var q = 16;
+// var r = 3;
+// var result = q * r;
+// console.log("The result of q * r after dividing q by 2 is: ", result);
+
+// // answer: 48;
+
+
+
+
+
+
+
+
+// Q11: if var s = 25 and var t = 3, what is the value of s / t after subtracting 1 from s ?
+
+// var s = 25;
+// var t = 3;
+
+// s -= 1;
+
+// var result = s / t;
+// console.log("The value of s / t after subtracting 1 from s is :", result);
+
+// answer: 8;
+
+
+
+
+
+
+
+// Q12: Suppose var u = 11 and var v = 3, calculate the value  of u - v after adding 2  to v?
+
+// var u = 11;
+// var v = 3;
+
+// v += 2;
+// var result = u - v;
+// console.log("The value of u - v after adding 2 to v is:", result);
+
+// answer : 6;
+
+
+
+
+
+
+
+// Q13:FOR  var w =22 and var x = 5, what will be the outcome of w + (x ** 2)?
+
+// var w = 22;
+// var x = 5;
+
+// var xSquard = Math.pow(x, 2);
+
+// var result = w + xSquard;
+// console.log("The outcome of w + xSquard (x ^ 2) is :", result);
+
+// answer: 47;
+
+
+
+
+
+
+// Q14: Given var y = 19 and var z = 4, determine the result of y % (z + 1)?
+
+// var  y = 19;
+// var  z = 4;
+// var result = y %(z+1);
+// console.log(result);
+
+// answer: 4;
+
+
+
+
+
+
+// Q15: if var a = 10 and var b = 3 , what is the value of a */ (b - 2 )?
+
+// var a = 10;
+// var b = 3;
+// var result = a * (b - 2);
+// console.log(result);
+
+// answer: 10;
